55cfba1e609111499d2b1308762004f03ed09046
mikoto <ele_comb@qq.com>
2018-05-16 23:04:54 +0800

    commit real Strassen use index of value

--- chapter_4_DivideStrategy/D_StrassenAlgorithm.py
+++ chapter_4_DivideStrategy/D_StrassenAlgorithm.py
@@ -14,22 +14,30 @@ def square_matrix_multiply_recursive(A, B,
     if n == 1:
         C[n - 1, n - 1] = A[L1[3], L1[0]] * B[L2[3], L2[0]]
     else:
-        A11 = [L1[0], L1[0] + d, L1[2], L1[2] + d]
-        A12 = [int((L1[0] + L1[1] + 1) / 2), L1[1], L1[2], L1[2] + d]
-        A21 = [L1[0], L1[0] + d, int((L1[2] + L1[3] + 1) / 2), L1[3]]
-        A22 = [int((L1[0] + L1[1] + 1) / 2), L1[1], int((L1[3] + L1[2] + 1) / 2), L1[3]]
-        B11 = [L2[0], L2[0] + d, L2[2], L2[2] + d]
-        B21 = [L2[0], L2[0] + d, int((L2[2] + L2[3] + 1) / 2), L2[3]]
-        B12 = [int((L2[0] + L2[1] + 1) / 2), L2[1], L2[2], L2[2] + d]
-        B22 = [int((L2[0] + L2[1] + 1) / 2), L2[1], int((L2[2] + L2[3] + 1) / 2), L2[3]]
-        C[0:d + 1, 0:d + 1] = square_matrix_multiply_recursive(A, B, A11, B11) + \
-                              square_matrix_multiply_recursive(A, B, A12, B21)
-        C[0:d + 1, int(n / 2):int(n / 2 + d + 1)] = square_matrix_multiply_recursive(A, B, A11, B12) + \
-                                                    square_matrix_multiply_recursive(A, B, A12, B22)
-        C[int(n / 2):int(n / 2 + d + 1), 0:d + 1] = square_matrix_multiply_recursive(A, B, A21, B11) + \
-                                                    square_matrix_multiply_recursive(A, B, A22, B21)
-        C[int(n / 2):int(n / 2 + d + 1), int(n / 2):int(n / 2 + d + 1)] = square_matrix_multiply_recursive(A, B, A21, B12) + \
-                                                                          square_matrix_multiply_recursive(A, B, A22, B22)
+        a11 = [L1[0], L1[0] + d, L1[2], L1[2] + d]
+        a12 = [int((L1[0] + L1[1] + 1) / 2), L1[1], L1[2], L1[2] + d]
+        a21 = [L1[0], L1[0] + d, int((L1[2] + L1[3] + 1) / 2), L1[3]]
+        a22 = [int((L1[0] + L1[1] + 1) / 2), L1[1], int((L1[3] + L1[2] + 1) / 2), L1[3]]
+        b11 = [L2[0], L2[0] + d, L2[2], L2[2] + d]
+        b21 = [L2[0], L2[0] + d, int((L2[2] + L2[3] + 1) / 2), L2[3]]
+        b12 = [int((L2[0] + L2[1] + 1) / 2), L2[1], L2[2], L2[2] + d]
+        b22 = [int((L2[0] + L2[1] + 1) / 2), L2[1], int((L2[2] + L2[3] + 1) / 2), L2[3]]
+
+        P1 = square_matrix_multiply_recursive(A, B, a11, b12) - square_matrix_multiply_recursive(A, B, a11, b22)
+        P2 = square_matrix_multiply_recursive(A, B, a11, b22) + square_matrix_multiply_recursive(A, B, a12, b22)
+        P3 = square_matrix_multiply_recursive(A, B, a21, b11) + square_matrix_multiply_recursive(A, B, a22, b11)
+        P4 = square_matrix_multiply_recursive(A, B, a22, b21) - square_matrix_multiply_recursive(A, B, a22, b11)
+        P5 = square_matrix_multiply_recursive(A, B, a11, b11) + square_matrix_multiply_recursive(A, B, a11, b22) + \
+             square_matrix_multiply_recursive(A, B, a22, b11) + square_matrix_multiply_recursive(A, B, a22, b22)
+        P6 = square_matrix_multiply_recursive(A, B, a12, b21) + square_matrix_multiply_recursive(A, B, a12, b22) - \
+             square_matrix_multiply_recursive(A, B, a22, b21) - square_matrix_multiply_recursive(A, B, a22, b22)
+        P7 = square_matrix_multiply_recursive(A, B, a11, b11) + square_matrix_multiply_recursive(A, B, a11, b12) - \
+             square_matrix_multiply_recursive(A, B, a21, b11) - square_matrix_multiply_recursive(A, B, a21, b12)
+
+        C[0:d + 1, 0:d + 1] = P5 + P4 - P2 + P6
+        C[0:d + 1, int(n / 2):int(n / 2 + d + 1)] = P1 + P2
+        C[int(n / 2):int(n / 2 + d + 1), 0:d + 1] = P3 + P4
+        C[int(n / 2):int(n / 2 + d + 1), int(n / 2):int(n / 2 + d + 1)] = P5 + P1 - P3 - P7
     return C
 
 
@@ -53,7 +61,8 @@ n = 8
 
 # A = [[1,2,3,4], [2,1,3,4], [4,3,2,1], [1,3,2,4]]
 # B = [[1,3,5,6], [3,5,1,6], [6,1,3,5], [5,1,6,3]]
-L1 = [0, n-1, 0, n-1]
-L2 = [0, n-1, 0, n-1]
+L1 = [0, n - 1, 0, n - 1]
+L2 = [0, n - 1, 0, n - 1]
 ret = square_matrix_multiply_recursive(A, B, L1, L2)
+# ret = np.dot(A, B)
 print(ret)
