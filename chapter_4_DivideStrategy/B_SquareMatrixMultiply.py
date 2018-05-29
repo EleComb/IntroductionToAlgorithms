50b95a459c0fa9c19f04cfe9f06318d4da4d35cc
mikoto <ele_comb@qq.com>
2018-05-29 19:05:47 +0800

    submit matrix multiply

--- chapter_4_DivideStrategy/B_SquareMatrixMultiply.py
+++ chapter_4_DivideStrategy/B_SquareMatrixMultiply.py
@@ -1,17 +1,21 @@
-
+import numpy as np
 
 def square_matrix_multiply(A, B):
-    n = len(A[0])
-    C = [[0 for col in range(n)] for row in range(n)]
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
+    col = len(A)
+    row = len(B[0])
+    C = [[0 for row in range(row)] for col in range(col)]
+    for i in range(col):
+        for j in range(row):
+            for k in range(len(B)):
                 C[i][j] += A[i][k]*B[k][j]
     return C
 
 
-# A = [[1,2], [2,1]]
-# B = [[1,3], [3,5]]
+# A = [[1, 2], [2, 1], [2, 5], [5, 2]]
+# B = [[1, 3, 2], [3, 5, 9]]
+# A = np.array(A)
+# B = np.array(B)
+# print(np.dot(A, B))
 # ret = square_matrix_multiply(A, B)
 # print(ret)
 
